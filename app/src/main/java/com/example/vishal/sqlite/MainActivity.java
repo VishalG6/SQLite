6cc9db14ab4ae0c18037eb1a0ee689fe26c74f7c
VishalG6 <vishalg_94@hotmail.com>
2016-01-09 22:57:18 +1100

    Working

--- app/src/main/java/com/example/vishal/sqlite/MainActivity.java
+++ app/src/main/java/com/example/vishal/sqlite/MainActivity.java
@@ -2,12 +2,46 @@ package com.example.vishal.sqlite;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText vishInput;
+    TextView vishText;
+    MyDBHandler dbHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        vishInput = (EditText) findViewById(R.id.vishInput);
+        vishText = (TextView) findViewById(R.id.vishText);
+        dbHandler = new MyDBHandler(this, null, null, 1);
+
+        printDatabase();
+
+     }
+
+    public void addButtonClicked (View view ){
+        Products products = new Products(vishInput.getText().toString());
+        dbHandler.addProduct(products);
+        printDatabase();
+
+    }
+
+    //Delete Item
+    public void deleteButtonClicked(View view){
+        String inputText = vishInput.getText().toString();
+        dbHandler.deleteProduct(inputText);
+        printDatabase();
+    }
+
+    public void printDatabase(){
+        String dbString = dbHandler.databasetoString();
+        vishText.setText(dbString);
+        vishInput.setText("");
+
     }
 }
--- app/src/main/java/com/example/vishal/sqlite/MyDBHandler.java
+++ app/src/main/java/com/example/vishal/sqlite/MyDBHandler.java
@@ -3,48 +3,76 @@ package com.example.vishal.sqlite;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class MyDBHandler extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "products.db"; // Name of database
+    private static final int DATABASE_VERSION = 1; // Database version
+    public static final String TABLE_PRODUCTS = "products";
+    public static final String COLUMN_ID = "_id";
+    public static final String COLUMN_PRODUCTNAME = "productname";
 
-    private static final int DATABASE_VERSION = 1;
-    private static final String DATABASE_NAME = "products.db";
-    public static final String TABLE_PRODUCTS ="products";
-    public static final String COLUME_ID ="_id";
-    public static final String COLUME_PRODUCTNAME ="_productname";
 
-    public MyDBHandler(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+
+    public MyDBHandler(Context context, String name, SQLiteDatabase.CursorFactory factory, int version){
+
         super(context, DATABASE_NAME, factory, DATABASE_VERSION);
     }
 
-    @Override
+
     public void onCreate(SQLiteDatabase db) {
-        String query = "CREATE TABLE" + TABLE_PRODUCTS + "(" +
-                COLUME_ID + " INTEGER PRIMARY KEY AUTOINCREMENT " +
-                COLUME_PRODUCTNAME + " TEXT " +
+        // create statement for making the table - the table is called inventory
+        String DATABASE_CREATE = "CREATE TABLE " + TABLE_PRODUCTS +"("+
+                COLUMN_ID +" INTEGER PRIMARY KEY AUTOINCREMENT ," +
+                COLUMN_PRODUCTNAME +" TEXT " +
                 ");";
-        db.execSQL(query);
+
+        db.execSQL(DATABASE_CREATE);
 
     }
 
-    @Override
+
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS" + TABLE_PRODUCTS);
-
+        db.execSQL("DROP TABLE IF EXISTS products");
+        onCreate(db);
     }
 
-    //Add a new row to the database
     public void addProduct(Products product){
         ContentValues values = new ContentValues();
-        values.put(COLUME_PRODUCTNAME, product,get_productname());
+        values.put(COLUMN_PRODUCTNAME, product.get_productname());
         SQLiteDatabase db = getWritableDatabase();
         db.insert(TABLE_PRODUCTS, null, values);
         db.close();
     }
 
-    //Delete a product from the database
-    public void deleteProduct (Products product);
-    SQLiteDatabase db = getWritableDatabase();
+    public void deleteProduct(String productName){
+        SQLiteDatabase db = getWritableDatabase();
+        db.execSQL("DELETE FROM " + TABLE_PRODUCTS + " WHERE " + COLUMN_PRODUCTNAME + "=\"" + productName + "\";");
+        db.close();
+    }
+
+    public String databasetoString(){
+        String dbString = "";
+        SQLiteDatabase db = getWritableDatabase();
+        String query = "SELECT * FROM '" + TABLE_PRODUCTS + "' WHERE 1;";
+
+        //CURSOR WILL POINT TO THE LOCTION IN THE RESULTS
+        Cursor c = db.rawQuery(query, null);
+        c.moveToFirst();
+
+        while(!c.isAfterLast()){
+            if(c.getString(c.getColumnIndex("productname"))!=null){
+                dbString += c.getString(c.getColumnIndex("productname"));
+                dbString += "\n";
+            }
+            c.moveToNext();
+        }
+        db.close();
+        return dbString;
+    }
 
 }
+
+
